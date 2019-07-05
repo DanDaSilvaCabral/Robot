6104abc6970bbfa2c18d331ea5e342eddacd74c2
DanDaSilvaCabral <dasilvacabrald@gmail.com>
2019-07-05 18:29:45 +0000

    ajout de get_bearing

--- robot.py
+++ robot.py
@@ -14,14 +14,14 @@ def takevid():
 
 	camera.resolution = (1024, 768)
 	camera.framerate = 15
-	with open('/home/pi/Desktop/tests/videos/count', 'r') as f:
+	with open('/home/pi/Desktop/Robot/videos/count', 'r') as f:
 		nb = f.read()
 
 	for i in range(6):
 		camera.start_preview()
-		camera.start_recording('/home/pi/Desktop/tests/videos/video' + nb + str(i) + '.h264')
+		camera.start_recording('/home/pi/Desktop/Robot/videos/video' + nb + str(i) + '.h264')
 		nb = int(nb) + 1
-		with open('/home/pi/Desktop/tests/videos/count', 'w') as f:
+		with open('/home/pi/Desktop/Robot/videos/count', 'w') as f:
 			f.write(str(nb))
 		sleep(1800)
 		camera.stop_recording()
@@ -56,10 +56,23 @@ def getGPS():
 	while stop != 1:
 		report = gpsd.next() #
 		if report['class'] == 'TPV':
-			cur_coord = {"lat": getattr(report,'lat',0.0), "lon": getattr(report,'lon',0.0), "time": getattr(report, 'time','')}
+			cur_coord = {"lat": getattr(report,'lat',0.0), "lon": getattr(report,'lon',0.0), "time": getattr(report, 'time',''), "alt": getattr(report, 'alt','nan')}
 			stop = 1
 			return cur_coord
 
 def get_bearing(lat1, lat2, long1, long2):
 	brng = Geodesic.WGS84.Inverse(lat1, long1, lat2, long2)['azi1']
 	return brng
+
+def cur_bearing():
+	cur_pos = getGPS()
+	try:
+		while True:
+			time.sleep(0.8)
+			new_pos = getGPS()
+			if new_pos != cur_pos:
+				brng = get_bearing(cur_pos['lat'], new_pos['lat'], cur_pos['lon'], new_pos['lon'])
+				print brng
+				cur_pos = new_pos
+	except (KeyboardInterrupt, SystemExit):
+		print "Done.\nExiting."
