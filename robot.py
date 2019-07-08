8738b0ce5dd3aa7167cee54160a69118d993e3dc
DanDaSilvaCabral <dasilvacabrald@gmail.com>
2019-07-08 16:08:06 +0000

    creation de la fonction current bearing

--- robot.py
+++ robot.py
@@ -5,6 +5,7 @@ from time import *
 import RPi.GPIO as GPIO
 from gps import *
 from geographiclib.geodesic import Geodesic
+import math
 
 def takevid():
 
@@ -14,14 +15,14 @@ def takevid():
 
 	camera.resolution = (1920, 1080)
 	camera.framerate = 15
-	with open('/home/pi/Desktop/Robot/videos/count', 'r') as f:
+	with open('/home/pi/Desktop/Robot/data/videos/count', 'r') as f:
 		nb = f.read()
 
 	for i in range(6):
 		camera.start_preview()
-		camera.start_recording('/home/pi/Desktop/Robot/videos/video' + nb + str(i) + '.h264')
+		camera.start_recording('/home/pi/Desktop/Robot/data/videos/video' + nb + str(i) + '.h264')
 		nb = int(nb) + 1
-		with open('/home/pi/Desktop/Robot/videos/count', 'w') as f:
+		with open('/home/pi/Desktop/Robot/data/videos/count', 'w') as f:
 			f.write(str(nb))
 		sleep(1800)
 		camera.stop_recording()
@@ -41,9 +42,9 @@ def printGPS():
 				print getattr(report,'lat',0.0),"\t",
 				print getattr(report,'lon',0.0),"\t",
 				print getattr(report,'time',''),"\t",
-				print getattr(report,'alt','nan'),"\t",
+			#	print getattr(report,'alt','nan'),"\t",
 	           		print  getattr(report,'speed','nan'),"\t",
-				print getattr(report,'climb','nan'),"\t"
+			#	print getattr(report,'climb','nan'),"\t"
 			time.sleep(1)
 
 	except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
@@ -56,7 +57,7 @@ def getGPS():
 	while stop != 1:
 		report = gpsd.next() #
 		if report['class'] == 'TPV':
-			cur_coord = {"lat": getattr(report,'lat',0.0), "lon": getattr(report,'lon',0.0), "time": getattr(report, 'time',''), "alt": getattr(report, 'alt','nan')}
+			cur_coord = {"lat": getattr(report,'lat',0.0), "lon": getattr(report,'lon',0.0), "time": getattr(report, 'time',''), "alt": getattr(report, 'alt','nan'), "speed": getattr(report, 'speed','nan')}
 			stop = 1
 			return cur_coord
 
@@ -64,16 +65,27 @@ def get_bearing(lat1, lat2, long1, long2):
 	brng = Geodesic.WGS84.Inverse(lat1, long1, lat2, long2)['azi1']
 	return brng
 
-def cur_bearing():
+def get_dist(lat1, lat2, long1, long2):
+	dist = Geodesic.WGS84.Inverse(lat1, long1, lat2, long2)['s12']
+	return dist
+
+def cur_bearing(targ_lat = 43.7, targ_lon = 7.26):
 	cur_pos = getGPS()
 	try:
 		while True:
-			time.sleep(0.8)
+			time.sleep(1)
 			new_pos = getGPS()
 			if new_pos != cur_pos:
 				brng = get_bearing(cur_pos['lat'], new_pos['lat'], cur_pos['lon'], new_pos['lon'])
-				with open('/home/pi/Desktop/Robot/record', 'a') as rec:
-					rec.write(str(brng)+'\n')
-				cur_pos = new_pos
+				dist = get_dist(cur_pos['lat'], new_pos['lat'], cur_pos['lon'], new_pos['lon'])
+				targ_brng = get_bearing(new_pos['lat'], targ_lat, new_pos['lon'], targ_lon)
+				dir = int(targ_brng) - int(brng)
+				if int(dist) > 3 and int(dist) < 5000:
+					with open('/home/pi/Desktop/Robot/data/record', 'a') as rec:
+						rec.write(str(brng)+'\t'+str(dist)+'\t'+str(dir)+'\n')
+					cur_pos = new_pos
 	except (KeyboardInterrupt, SystemExit):
 		print "Done.\nExiting."
+
+
+
